4de69f1f102c8beb4acf680c2f27c93209b41ed8
Jason Jarrett <jason@elegantcode.com>
2015-06-20 22:25:23 -0700

    Updated site: Sun, 21 Jun 2015 05:25:23 GMT

--- js/octopress.js
+++ js/octopress.js
@@ -13,17 +13,17 @@ function getNav() {
 }
 
 function addSidebarToggler() {
-  if(!$('body').hasClass('sidebar-footer')) {
-    $('#content').append('<span class="toggle-sidebar"></span>');
-    $('.toggle-sidebar').bind('click', function(e) {
-      e.preventDefault();
-      if ($('body').hasClass('collapse-sidebar')) {
-        $('body').removeClass('collapse-sidebar');
-      } else {
-        $('body').addClass('collapse-sidebar');
-      }
-    });
-  }
+  // if(!$('body').hasClass('sidebar-footer')) {
+  //   $('#content').append('<span class="toggle-sidebar"></span>');
+  //   $('.toggle-sidebar').bind('click', function(e) {
+  //     e.preventDefault();
+  //     if ($('body').hasClass('collapse-sidebar')) {
+  //       $('body').removeClass('collapse-sidebar');
+  //     } else {
+  //       $('body').addClass('collapse-sidebar');
+  //     }
+  //   });
+  // }
   var sections = $('aside.sidebar > section');
   if (sections.length > 1) {
     sections.each(function(index, section){
