f808faa129d0f9e73ecc3dca09dc25ea5b8cd0c7
Jason Jarrett <jason@elegantcode.com>
2017-05-01 13:29:11 -0700

    Updated site: Mon, 01 May 2017 20:29:10 GMT

--- ExceptionMessageBeautifier/javascripts/main.js
+++ ExceptionMessageBeautifier/javascripts/main.js
@@ -10,15 +10,38 @@ if (!exports && !exports.format) {
 
 formatException = exports.format;
 
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
+
 
 function ExBeautifierCtrl($scope) {
+
+    var passedEx = getQueryVariable('ex');
+    $scope.exInput = passedEx;
 
     $scope.shouldShowExample = false;
     $scope.showExample = function () {
         $scope.shouldShowExample = !$scope.shouldShowExample;
     }
     $scope.$watch('exInput', function () {
-        $scope.exOutput = formatException($scope.exInput);
+        var input = $scope.exInput || '';
+
+        $scope.exOutput = formatException(input);
+
+        var url = location.origin + location.pathname;
+        if (input.trim()) {
+            url += "?ex=" + encodeURIComponent(input);
+        }
+        history.replaceState(null, '', url)
     });
 
 }
